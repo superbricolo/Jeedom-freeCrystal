ef2b696ef29643d5ea80ca69af2f4c258519dbb1
Halbout Michael <michaelhalbout@gmail.com>
2016-09-29 14:54:58 +0200

    Update configuration.php

--- plugin_info/configuration.php
+++ plugin_info/configuration.php
@@ -6,28 +6,14 @@ if (!isConnect()) {
     die();
 }
 ?>
-
 <form class="form-horizontal">
     <fieldset>
 		<div class="form-group">
-			<label class="col-lg-4 control-label">Rafraichissement des informations Principal :</label>
+			<label class="col-lg-4 control-label">Délais de afraichissement des informations :</label>
 			<div class="col-lg-4">
-				<input class="configKey form-control" data-l1key="RefreshData" id="RefreshData"/>
-				<a class="btn btn-default btn-xs cursor" style="position : relative; top : 3px;" title="Générateur Cron">
-					<i class="fa fa-list-alt"></i>
-				</a>
+				<input class="configKey form-control" data-l1key="DemonSleep"/>
 			</div>
 		</div> 
    </fieldset>
 </form>
-<script>
-$(function() 
-	{
-  $('.form-group').delegate('.btn', 'click', function() {
-	  	var _this=this;
-		jeedom.getCronSelectModal({},function (result) {
-			$('#RefreshData').val(result.value);
-			}); 
-		}); 
-	});
-</script>
+
