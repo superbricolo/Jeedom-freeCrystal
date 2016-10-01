7568f9789bde563adbe4fac22a620f556360d14e
Halbout Michael <michaelhalbout@gmail.com>
2016-10-01 08:01:07 +0200

    Update freeCrystal.php

--- desktop/php/freeCrystal.php
+++ desktop/php/freeCrystal.php
@@ -31,7 +31,7 @@ $eqLogics = eqLogic::byType('freeCrystal');
                 foreach ($eqLogics as $eqLogic) {
                     echo '<div class="eqLogicDisplayCard cursor" data-eqLogic_id="' . $eqLogic->getId() . '" style="background-color : #ffffff; height : 200px;margin-bottom : 10px;padding : 5px;border-radius: 2px;width : 160px;margin-left : 10px;" >';
                     echo "<center>";
-                    echo '<img src="plugins/Freebox_OS/doc/images/Freebox_OS_icon.png" height="105" width="95" />';
+                    echo '<img src="plugins/freeCrystal/doc/images/freeCrystal_icon.png" height="105" width="95" />';
                     echo "</center>";
                     echo '<span style="font-size : 1.1em;position:relative; top : 15px;word-break: break-all;white-space: pre-wrap;word-wrap: break-word;"><center>' . $eqLogic->getHumanName(true, true) . '</center></span>';
                     echo '</div>';
