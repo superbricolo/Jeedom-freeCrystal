f409cf075d9f675b4f6f71e68b40d0abb08621cf
Halbout Michael <michaelhalbout@gmail.com>
2016-10-07 07:59:33 +0200

    Update configuration.php

--- plugin_info/configuration.php
+++ plugin_info/configuration.php
@@ -9,13 +9,13 @@ if (!isConnect()) {
 <form class="form-horizontal">
     <fieldset>
 		<div class="form-group">
-			<label class="col-lg-4 control-label">Délais de afraichissement des informations (s) :</label>
+			<label class="col-lg-4 control-label">Délais de rafraîchissement des informations (s) :</label>
 			<div class="col-lg-4">
 				<input class="configKey form-control" data-l1key="DemonSleep"/>
 			</div>
 		</div> 
 		<div class="form-group">
-			<label class="col-lg-4 control-label">Code telecommande :</label>
+			<label class="col-lg-4 control-label">Code télécommande :</label>
 			<div class="col-lg-4">
 				<input class="configKey form-control" data-l1key="Code"/>
 			</div>
