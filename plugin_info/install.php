d204f39f92ff5704fdd9f7f91e787bb870e2afe3
Halbout Michael <michaelhalbout@gmail.com>
2016-10-07 15:27:09 +0200

    Update install.php

--- plugin_info/install.php
+++ plugin_info/install.php
@@ -3,13 +3,22 @@ require_once dirname(__FILE__) . '/../../../core/php/core.inc.php';
 
 function freeCrystal_install() {
 	$equipement=eqLogic::byLogicalId('Reseau','freeCrystal');
-	$Commande=$equipement->getCmd(null,'RéponsePing');
-	$Commande->remove();
+	if(is_object($equipement)){
+		$Commande=$equipement->getCmd(null,'RéponsePing');
+		$Commande->remove();
+		$Commande=$equipement->getCmd(null,'AdresseIPprivee');
+		$Commande->remove();
+	}
+							
 }
 function freeCrystal_update() {
 	$equipement=eqLogic::byLogicalId('Reseau','freeCrystal');
-	$Commande=$equipement->getCmd(null,'RéponsePing');
-	$Commande->remove();
+	if(is_object($equipement)){
+		$Commande=$equipement->getCmd(null,'RéponsePing');
+		$Commande->remove();
+		$Commande=$equipement->getCmd(null,'AdresseIPprivee');
+		$Commande->remove();
+	}
 }
 
 function freeCrystal_remove() {
